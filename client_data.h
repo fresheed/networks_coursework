e0f3a456d68571ebc0312ecd919d53e1fb998ae4
fresheed <ok.john.rus@gmail.com>
2016-10-11 22:47:46 +0300

    removed unused files

--- client_data.h
+++
@@ -1,5 +0,0 @@
-struct client_data {
-  char name[20];
-  unsigned int socket_fd;
-  pthread_t thread;
-};
--- logs.c
+++
@@ -1,8 +0,0 @@
-#include <pthread.h>
-#include <stdio.h>
-
-void threadLog(){
-  pthread_t this_thread=pthread_self();
-  unsigned int tid=(unsigned int)this_thread;
-  printf("Thread %d:", tid);
-}
