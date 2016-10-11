df5f6569f5b4e08bfb6bc8117d88140da7c253f3
fresheed <ok.john.rus@gmail.com>
2016-10-11 22:48:16 +0300

    removing unused

--- init_sockets.h
+++
@@ -1,8 +0,0 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-
-
-void setServerAddressParams(struct sockaddr_in* server_address, int port);
-int prepareServerSocket();
-int acceptClient(int server_socket_fd);
--- win/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "client.c"
-
-int main(int argc, char* argv[])
-{
-    run_client();
-    return 0;
-}
