64a1b6de0dbc7a4b3861f9a0654fcccd4cbf5364
fresheed <ok.john.rus@gmail.com>
2016-10-27 00:15:27 +0300

    updated recompile

--- recompile.sh
+++ recompile.sh
@@ -6,4 +6,4 @@ gcc primes_server.c nodes_processing.c server_threads.c messages.c init_sockets.
 
 rm primes_node
 echo Compiling node:
-gcc primes_node.c init_sockets.c -o primes_node
+gcc primes_node.c init_sockets.c messages.c server_threads.c -o primes_node -pthread
